474cb14ab9c325113ad71819bf499682f6ad719a
网易羞羞 <yixiuwan@usc.edu>
2025-12-24 22:49:47 -0800

    Deploy

--- src/components/FooterBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class="border-t">
-    <div class="max-w-6xl mx-auto px-4 py-6 text-sm opacity-70">
-      © {{ new Date().getFullYear() }} Yixiu Wang · Built with Vue & Vite
-    </div>
-  </footer>
-</template>
